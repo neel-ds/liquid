41d451a440e12f65fece3a48309040160ca9bfe7
Neel <neelp0980@gmail.com>
2024-01-14 16:51:30 +0530

    style: NavBar comp

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Navbar from "@/components/NavBar";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar />
+      <div className="min-h-[calc(100vh-68px)] pt-16 px-8 sm:px-28 md:px-32 lg:px-48">
+        <Component {...pageProps} />
+      </div>
+    </>
+  );
 }
